b94b6c89b4f74b748f0256ecc3f8a7f68d7c8286
Michael Saiba <michaelsaiba84@gmail.com>
2022-07-04 05:44:23 +0300

    Changes

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,27 @@
   <div>
     <MainMenu />
     <HeroSection />
+    <HighLights />
+    <UseCases />
+    <ContactUs />
   </div>
 </template>
 
 <script>
   import MainMenu from "./components/MainMenu.vue";
   import HeroSection from "./components/HeroSection.vue";
+  import HighLights from "./components/HighLights.vue";
+  import UseCases from "./components/UseCases.vue";
+  import ContactUs from "./components/ContactUs.vue";
 
   export default {
     name: "App",
     components: {
       MainMenu,
       HeroSection,
+      HighLights,
+      UseCases,
+      ContactUs,
     },
   };
 </script>
---
+++ src/components/HighLights.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>
+    <div class="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div
+        class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl"
+      >
+        <div>
+          <h2
+            class="text-3xl tracking-tight font-extrabold text-black sm:text-4xl"
+          >
+            Highlights
+          </h2>
+        </div>
+        <div
+          class="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12"
+        >
+          <div>
+            <a href="#" class="mt-2 block">
+              <p class="text-xl font-semibold text-gray-900">
+                Rely on your own impartial data for correct estimates, site
+                analysis and payments
+              </p>
+              <span class="mt-3 text-base text-gray-500">
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Accurately measure volume, areas, heights, points and
+                  distances using survey grade accuracy.
+                </p>
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Evaluate construction progress and validate work with
+                  Earthworks analysis.
+                </p>
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Analyze construction deviation.
+                </p>
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  3D Cross-section analysis, plotting and export.
+                </p>
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Export measurements (points, distances,areas and volumes) in
+                  common GIS and CAD formats.
+                </p>
+              </span>
+            </a>
+          </div>
+
+          <div>
+            <a href="#" class="mt-2 block">
+              <p class="text-xl font-semibold text-gray-900">
+                Perform Inspection remotely
+              </p>
+              <span class="mt-3 text-base text-gray-500">
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Conduct detailed inspection and analysis from high-resolution
+                  imagery in a fraction of the time, reducing downtime.
+                </p>
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Inspect hard-to-reach areas without putting your team in
+                  danger.
+                </p>
+              </span>
+            </a>
+          </div>
+
+          <div>
+            <a href="#" class="mt-2 block">
+              <p class="text-xl font-semibold text-gray-900">
+                Unify & Explore your site Data in one platform
+              </p>
+              <span class="mt-3 text-base text-gray-500">
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Compare as-built to as-designed with design plan overlays
+                </p>
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Compare different data sets to gather rich insights over time.
+                </p>
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Visualize contours,2D as built data, 3D meshes, pointclouds,
+                  orthomosaic maps and digital elevation models in one platform.
+                </p>
+              </span>
+            </a>
+          </div>
+
+          <div>
+            <a href="#" class="mt-2 block">
+              <p class="text-xl font-semibold text-gray-900">
+                Collaborate from Anywhere using Scalable Cloud Infrastructure
+              </p>
+              <span class="mt-3 text-base text-gray-500">
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Explore maps, photos on any device.
+                </p>
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Receive updates and access the latest data.
+                </p>
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Connect your team with an interactive, up-to-date visual
+                  record of your project
+                </p>
+              </span>
+            </a>
+          </div>
+
+          <div>
+            <a href="#" class="mt-2 block">
+              <p class="text-xl font-semibold text-gray-900">Data Security</p>
+              <span class="mt-3 text-base text-gray-500">
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  We are committed to having a robust security program in place
+                  that puts the security and privacy of your data as our top
+                  priority.
+                </p>
+              </span>
+            </a>
+          </div>
+          <div>
+            <a href="#" class="mt-2 block">
+              <p class="text-xl font-semibold text-gray-900">
+                Share data with your organization & clients
+              </p>
+              <span class="mt-3 text-base text-gray-500">
+                <p class="mt-2">
+                  <i class="fas fa-caret-right mr-4 text-polar-red"></i>
+                  Give instant visibility to your data and status of your
+                  project. Our solution enables you to share your data with
+                  ease, while controlling the level of access to individual
+                  projects.
+                </p>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HighLights",
+    props: {},
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>

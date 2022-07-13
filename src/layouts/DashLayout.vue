e032afa8b8d2a24f30aa6c2b3ac564809bd907a8
Michael Saiba <michaelsaiba84@gmail.com>
2022-07-14 02:50:20 +0300

    Auth, Dash, Routes, Layout Desing

--- src/layouts/DashLayout.vue
+++ src/layouts/DashLayout.vue
@@ -436,12 +436,11 @@
               </form>
             </div>
             <div class="ml-4 flex items-center md:ml-6">
-              <button
+              <!-- <button
                 type="button"
                 class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span class="sr-only">View notifications</span>
-                <!-- Heroicon name: outline/bell -->
                 <svg
                   class="h-6 w-6"
                   xmlns="http://www.w3.org/2000/svg"
@@ -457,7 +456,7 @@
                     d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                   />
                 </svg>
-              </button>
+              </button> -->
 
               <!-- Profile dropdown -->
               <div class="ml-3 relative">
@@ -465,17 +464,18 @@
                   <button
                     @click="userSettings = !userSettings"
                     type="button"
-                    class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    class="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-polar-red"
                     id="user-menu-button"
                     aria-expanded="false"
                     aria-haspopup="true"
                   >
                     <span class="sr-only">Open user menu</span>
-                    <img
+                    <i class="fas fa-cog text-polar-red fa-2x"></i>
+                    <!-- <img
                       class="h-8 w-8 rounded-full"
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                       alt=""
-                    />
+                    /> -->
                   </button>
                 </div>
 

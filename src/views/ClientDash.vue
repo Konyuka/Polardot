d3f14582008c2db42b4fc32eb14b29ba3c49087e
Michael Saiba <michaelsaiba84@gmail.com>
2022-07-15 11:49:15 +0300

    Stripe added

--- src/views/ClientDash.vue
+++ src/views/ClientDash.vue
@@ -479,11 +479,175 @@
         </div>
       </div>
     </div>
+
+    <!-- modal start -->
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div
+      class="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <!--
+    Background backdrop, show/hide based on modal state.
+
+    Entering: "ease-out duration-300"
+      From: "opacity-0"
+      To: "opacity-100"
+    Leaving: "ease-in duration-200"
+      From: "opacity-100"
+      To: "opacity-0"
+  -->
+      <div
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+      ></div>
+
+      <div class="fixed z-10 inset-0 overflow-y-auto">
+        <div
+          class="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0"
+        >
+          <!--
+        Modal panel, show/hide based on modal state.
+
+        Entering: "ease-out duration-300"
+          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100 translate-y-0 sm:scale-100"
+          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      -->
+          <div
+            class="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6"
+          >
+            <div>
+              <div
+                class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100"
+              >
+                <!-- Heroicon name: outline/check -->
+                <svg
+                  class="h-6 w-6 text-green-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+              </div>
+              <div class="mt-3 text-center sm:mt-5">
+                <h3
+                  class="text-lg leading-6 font-medium text-gray-900"
+                  id="modal-title"
+                >
+                  Payment successful
+                </h3>
+                <!-- v-slot="{ elements, instance }" -->
+                <StripeElements
+                  v-if="stripeLoaded"
+                  v-slot="{ elements }"
+                  ref="elms"
+                  :stripe-key="stripeKey"
+                  :instance-options="instanceOptions"
+                  :elements-options="elementsOptions"
+                >
+                  <StripeElement
+                    ref="card"
+                    :elements="elements"
+                    :options="cardOptions"
+                  />
+                </StripeElements>
+                <button type="button" @click="pay">Pay</button>
+              </div>
+            </div>
+            <div
+              class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense"
+            >
+              <button
+                type="button"
+                class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+              >
+                Deactivate
+              </button>
+              <button
+                type="button"
+                class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- modal end -->
   </DashLayout>
 </template>
 
 <script setup>
   import DashLayout from "@/layouts/DashLayout.vue";
+  import { loadStripe } from "@stripe/stripe-js";
+  import { ref, onBeforeMount } from "vue";
+  import { StripeElements, StripeElement } from "vue-stripe-js";
+
+  // components: {
+  //   StripeElements,
+  //   StripeElement,
+  // },
+
+  // onBeforeMount(() => {
+  //     const stripeLoaded = ref(false);
+  //     const stripePromise = loadStripe("your_key");
+  //     stripePromise.then(() => {
+  //       stripeLoaded.value = true;
+  //     });
+  // });
+
+  const stripeKey = ref("pk_test_wRtooaqqYLA8Wlf3BFOksesO00z1idkYdN"); // test key
+  const instanceOptions = ref({
+    // https://stripe.com/docs/js/initializing#init_stripe_js-options
+  });
+  const elementsOptions = ref({
+    // https://stripe.com/docs/js/elements_object/create#stripe_elements-options
+  });
+  const cardOptions = ref({
+    // https://stripe.com/docs/stripe.js#element-options
+    value: {
+      postalCode: "12345",
+    },
+  });
+  const stripeLoaded = ref(false);
+  const card = ref();
+  const elms = ref();
+
+  function pay() {
+    // console.log(card.value);
+    let cardDetail = card.value;
+    let elmsDetail = elms.value;
+    // Get stripe element
+    const cardElement = cardDetail.stripeElement;
+    // const cardElement = this.card.stripeElement;
+
+    // Access instance methods, e.g. createToken()
+    elmsDetail.instance.createToken(cardElement).then((result) => {
+      // this.elms.instance.createToken(cardElement).then((result) => {
+      // Handle result.error or result.token
+      console.log(result);
+    });
+  }
+
+  onBeforeMount(() => {
+    const stripePromise = loadStripe(stripeKey.value);
+    stripePromise.then(() => {
+      stripeLoaded.value = true;
+    });
+  });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
